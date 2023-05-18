6ac16354a2821751f920067bc8e08f461147a7ea
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-05-18 14:45:55 +0200

    Add pull_singularity_containers.sh

---
+++ pull_singularity_containers.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -o pipefail
+set -u
+set -x
+
+mkdir -p containers/cache
+
+read -r -d '' -a uris < <(find configs -type f -exec grep 'container[[:space:]]*=[[:space:]]' {} + |
+                          sed -E "s|.*container[[:space:]]+=[[:space:]]+[\"'](.*)[\"']|\1|" |
+                          sort -u && printf '\0')
+
+echo "uris: ${uris[*]}"
+
+for uri in "${uris[@]}"; do
+    name="$(echo "$uri" | tr  -c '[:alnum:]_.\n' '-').img"
+    singularity pull --disable-cache -F --name "containers/cache/$name" "docker://$uri" &> /dev/null \
+    && echo "Done processing $uri..." &
+done
+
+echo "Waiting for pulls to complete..."
+wait
+echo "DONE!"
